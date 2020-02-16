628e6352043cffa136ebc7377220aa9596b9a4b8
17 <is_17Wang@163.com>
2020-02-16 17:41:42 +0800

    [feat] 初始化管理后台和基础模型

--- leeyum/admin.py
+++ leeyum/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from leeyum.domain import models
 
-# Register your models here.
+admin.site.site_header = '流云校园'
+admin.site.site_title = '流云校园管理后台'
+
+
+@admin.register(models.ArticleStore)
+class ArticleAdmin(admin.ModelAdmin):
+    # list_display 设置要显示在列表中的字段（id字段是Django模型的默认主键）
+    # list_per_page 设置每页显示多少条记录，默认是100条
+    # ordering 设置默认排序字段，负号表示降序排序
+    # list_editable 设置默认可编辑字段
+    # fk_fields 设置显示外键字段
+    # list_display_links 设置哪些字段可以点击进入编辑界面
+    # list_filter 筛选器
+    # search_fields 搜索器
+    @staticmethod
+    def tags(obj):
+        return [item.name for item in obj.tags.all()]
+
+    list_display = ('id', 'title', 'pic_urls', 'content', 'tags', 'category')
+    filter_horizontal = ('tags',)
+
+
+@admin.register(models.CommentStore)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.TagStore)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.CategoryStore)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- leeyum/domain/models.py
+++ leeyum/domain/models.py
@@ -1,26 +1,25 @@
 from django.db import models
 
 
+class ObjectStatus(object):
+    normal = 0
+    deleted = -1
+
+
 # Create your models here.
 class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
     gmt_modified = models.DateTimeField('修改时间')
     gmt_created = models.DateTimeField('创建时间')
 
 
-class ArticleStore(BaseModel):
-    """
-    信息模块
-    """
+class UserStore(BaseModel):
+    class Meta:
+        db_table = "leeyum_user"
+
     pass
-
-
-class CommentStore(BaseModel):
-    """
-    评论系统
-    """
-    comment_parents = models.IntegerField('评论回复上层id', default=0)
-    comment_message = models.CharField('评论信息', max_length=1024)
-    comment_publisher = models.IntegerField('发布人id', default=-1)
 
 
 # class UserViewRel(BaseModel):
@@ -43,18 +42,61 @@ class TagStore(BaseModel):
     """
     标签
     """
+    class Meta:
+        db_table = "leeyum_tag"
+
     name = models.CharField('标签名字', max_length=128)
     intro = models.CharField('标签介绍', max_length=256)
+
+    def __str__(self):
+        return self.name
 
 
 class CategoryStore(BaseModel):
     """
     类目
     """
+    class Meta:
+        db_table = "leeyum_category"
+
     name = models.CharField('类目名字', max_length=128)
     intro = models.CharField('类目介绍', max_length=256)
     parent = models.IntegerField('上级id', default=0)
 
+    def __str__(self):
+        return self.name
 
-# class UserStore(BaseModel):
-#     pass
+
+class ArticleStore(BaseModel):
+    """
+    信息模块
+    """
+    class Meta:
+        db_table = "leeyum_article"
+
+    title = models.CharField('标题', max_length=1024)
+    pic_urls = models.CharField('图片url', max_length=2048)
+    content = models.CharField('详情内容', max_length=1024)
+
+    tags = models.ManyToManyField(TagStore)
+    category = models.ForeignKey(CategoryStore, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class CommentStore(BaseModel):
+    """
+    评论系统
+    """
+    class Meta:
+        db_table = "leeyum_comment"
+
+    comment_parents = models.IntegerField('评论回复上层id', default=0)
+    comment_message = models.CharField('评论信息', max_length=1024)
+
+    comment_publisher = models.ForeignKey(UserStore, on_delete=models.CASCADE)
+    comment_article = models.ForeignKey(ArticleStore, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment_message
