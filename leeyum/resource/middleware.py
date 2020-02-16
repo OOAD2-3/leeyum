542aa10cd059df62dfbe3c89272024a33e988ff6
17 <is_17Wang@163.com>
2020-02-16 23:59:41 +0800

    [feat] 判断是否处于登录态

--- leeyum/resource/middleware.py
+++ leeyum/resource/middleware.py
@@ -1,7 +1,22 @@
+import re
+
+from django.core.exceptions import PermissionDenied
 from django.utils.deprecation import MiddlewareMixin
 
 
 class CtxMiddleware(MiddlewareMixin):
+    TRUST_URL = ['/admin/', '/api/user/captcha', '/api/user/login']
+
     def process_request(self, request):
-        # TODO 判断登录态 非登录用户（无session信息，即request.user为AnonymousUser）
-        return
+        if self.is_permission_path(request.path):
+            return
+
+        # 不处于登录态的不给登录
+        if not request.user.is_authenticated:
+            raise PermissionDenied('your have no permission')
+
+    def is_permission_path(self, path):
+        for pattens in self.TRUST_URL:
+            if re.match(pattens, path):
+                return True
+        return False
