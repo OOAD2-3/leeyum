084e34305ed39e81b1dc74f41dc49ac8552084e3
17 <is_17Wang@163.com>
2020-02-19 16:50:46 +0800

    [fix] 允许host

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '6%$pf5*e#98yu3+s1p52g#iwm9+kv(mva1o+l0y7kid*y4-jcs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
